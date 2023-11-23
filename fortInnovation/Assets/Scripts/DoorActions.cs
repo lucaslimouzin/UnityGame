3bc93aab4dfebaa89d56cd620b26e7fc5d99132f
Givros <limouzin.lucas@gmail.com>
2023-11-23 12:56:46 +0100

    ajout textures room

---
+++ fortInnovation/Assets/Scripts/DoorActions.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class DoorActions : MonoBehaviour
+{
+     public GameObject panelDoor;
+    public float distanceDuDeplacement = 5f; // Distance à déplacer sur l'axe Y
+    public float dureeDuDeplacement = 3f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        #if !UNITY_EDITOR && UNITY_WEBGL
+            // disable WebGLInput.stickyCursorLock so if the browser unlocks the cursor (with the ESC key) the cursor will unlock in Unity
+            WebGLInput.stickyCursorLock = true;
+        #endif
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.gameObject.CompareTag("Player")){
+            panelDoor.SetActive(true);
+            //Set Cursor to not be visible
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None; 
+        }
+    }
+
+    private void OnTriggerExit(Collider other) {
+        if (other.gameObject.CompareTag("Player")){
+            if (panelDoor.activeSelf){
+                panelDoor.SetActive(false);
+                //Set Cursor to not be visible
+                Cursor.visible = false;
+                Cursor.lockState = CursorLockMode.Locked;
+            }
+        }
+    }
+
+     public void PlayGameBaton() {
+        panelDoor.SetActive(false);
+        //Set Cursor to not be visible
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        //mettre un delai
+        StartCoroutine(DeplacerGameObject());
+        Invoke("ChangerDeSceneVersBaton", 2f);
+        
+    }
+
+    private void ChangerDeSceneVersBaton(){
+        SceneManager.LoadScene("jeuBatonQuestions");
+    }
+
+
+    private IEnumerator DeplacerGameObject()
+    {
+        Vector3 startPosition = transform.position;
+        Vector3 targetPosition = startPosition + new Vector3(0f, distanceDuDeplacement, 0f);
+        float elapsedTime = 0f;
+
+        while (elapsedTime < dureeDuDeplacement)
+        {
+            // Interpoler la position entre le point de départ et la position cible
+            transform.position = Vector3.Lerp(startPosition, targetPosition, elapsedTime / dureeDuDeplacement);
+
+            // Mettre à jour le temps écoulé
+            elapsedTime += Time.deltaTime;
+
+            // Attendre la prochaine trame
+            yield return null;
+        }
+
+        // Assurez-vous que l'objet soit bien positionné à la position cible à la fin
+        transform.position = targetPosition;
+
+       
+    }
+
+
+
+
+}
--- fortInnovation/Assets/Scripts/GameManagerBaton.cs
+++ fortInnovation/Assets/Scripts/GameManagerBaton.cs
@@ -51,6 +51,7 @@ public class GameManagerBaton : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Cursor.visible = true;
         Cursor.lockState = CursorLockMode.None;
         
         // Charger le fichier JSON (assurez-vous de placer le fichier dans le dossier Resources)
