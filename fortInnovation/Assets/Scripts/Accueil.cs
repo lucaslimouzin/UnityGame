6e6dcaad83c2f4ea1f7482bd6d419b3cde1454be
Givros <69457849+lucaslimouzin@users.noreply.github.com>
2023-12-29 11:42:59 +0100

    ajout du personnage dans toutes les scènes

--- fortInnovation/Assets/Scripts/Accueil.cs
+++ fortInnovation/Assets/Scripts/Accueil.cs
@@ -2,13 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 public class Accueil : MonoBehaviour
 {
-    public GameObject[] characters;
+    public Image[] characters;
     public int selectedCharacter =0;
+
+    void Start(){
+        characters[0].enabled = true;
+        characters[1].enabled = false;
+    }
+    public void NextCharacter(){
+        characters[selectedCharacter].enabled = false;
+        selectedCharacter = (selectedCharacter +1 ) % characters.Length;
+        characters[selectedCharacter].enabled = true;
+    }
+
+    public void PreviousCharacter(){
+        characters[selectedCharacter].enabled = false;
+        selectedCharacter--;
+        if (selectedCharacter <0){
+            selectedCharacter += characters.Length;
+        }
+        characters[selectedCharacter].enabled = true;
+    }
+
     public void PlayGame() {
         MainGameManager.Instance.selectedCharacter = selectedCharacter;
-        SceneManager.LoadScene("Third_FortAccueil");
+        SceneManager.LoadScene("FortAccueil");
         Debug.Log("test");
     }
 }
