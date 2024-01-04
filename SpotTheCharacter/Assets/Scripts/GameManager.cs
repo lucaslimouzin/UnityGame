f094901957c85bbebb4cadb2932f5e2b4a5d28a3
Givros <69457849+lucaslimouzin@users.noreply.github.com>
2024-01-04 16:32:20 +0100

    ajout du textlvl qui change dynamiquement

--- SpotTheCharacter/Assets/Scripts/GameManager.cs
+++ SpotTheCharacter/Assets/Scripts/GameManager.cs
@@ -2,23 +2,56 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement; // Nécessaire pour accéder aux informations de la scène
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
     public GameObject spoonyPrefab;
     public Sprite[] bodySprites, faceSprites, brasDroitSprites, brasGaucheSprites;
     public Image uiCorps, uiFace, uiBrasDroit, uiBrasGauche; // Références aux éléments UI 
+    public TMP_Text levelText;
 
     private GameObject selectedSpoonyForUI;
 
     void Start()
     {
-        GameObject spoony1 = SpawnSpoony();
-        GameObject spoony2 = SpawnSpoony(); // Spawn deux fois
+        string sceneName = SceneManager.GetActiveScene().name;
 
-        // Sélectionnez aléatoirement l'un des deux Spoonies
-        selectedSpoonyForUI = Random.value < 0.5 ? spoony1 : spoony2;
+        // Supposer que le nom de la scène est toujours sous la forme "Level_XXX"
+        if (sceneName.StartsWith("Level_"))
+        {
+            string levelNumberStr = sceneName.Substring("Level_".Length);
+            if (int.TryParse(levelNumberStr, out int levelNumber))
+            {
+                levelText.text = "Level " + levelNumber; // Affiche "Level X" où X est le numéro de la scène
+            }
+        }
+
+        int numberOfSpoonies = DetermineNumberOfSpoonies();
+        List<GameObject> spawnedSpoonies = new List<GameObject>();
+
+        for (int i = 0; i < numberOfSpoonies; i++)
+        {
+            spawnedSpoonies.Add(SpawnSpoony());
+        }
+
+        // Sélectionnez aléatoirement l'un des Spoonies
+        selectedSpoonyForUI = spawnedSpoonies[Random.Range(0, spawnedSpoonies.Count)];
         UpdateUI(selectedSpoonyForUI);
+    }
+
+    int DetermineNumberOfSpoonies()
+    {
+        string sceneName = SceneManager.GetActiveScene().name;
+        if (sceneName.StartsWith("Level_"))
+        {
+            if (int.TryParse(sceneName.Substring("Level_".Length), out int levelNumber))
+            {
+                return levelNumber + 1; // Par exemple Level_001 pour 2 Spoonies
+            }
+        }
+        return 2; // Valeur par défaut si le nom de la scène ne correspond pas
     }
 
     GameObject SpawnSpoony()
