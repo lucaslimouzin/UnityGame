26fa20f7b8224d61da72789478807a3e2eb4b70c
Givros <69457849+lucaslimouzin@users.noreply.github.com>
2024-01-04 15:09:48 +0100

    double tap ajouté

--- SpotTheCharacter/Assets/Scripts/SpoonyScript.cs
+++ SpotTheCharacter/Assets/Scripts/SpoonyScript.cs
@@ -5,13 +5,38 @@ using UnityEngine.EventSystems;
 
 public class SpoonyScript : MonoBehaviour
 {
-    private void OnMouseDrag() {
-        transform.position = GetMousePos();
+     private Vector3 offset;
+    private Camera mainCamera;
+    private float lastClickTime;
+    private const float doubleClickThreshold = 0.2f; // Temps en secondes
+
+
+    void Start()
+    {
+        mainCamera = Camera.main; // Obtenir une référence à la caméra principale
     }
 
-    Vector3 GetMousePos(){
-        var mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        mousePos.z = 0;
-        return mousePos;
+    
+    void OnMouseDown()
+    {
+        // Convertir la position de l'objet de l'espace monde en espace écran
+        Vector3 objectPointInScreen = mainCamera.WorldToScreenPoint(transform.position);
+
+        // Calculer l'offset entre la position de l'objet et la position de la souris/du doigt
+        offset = transform.position - mainCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, objectPointInScreen.z));
+    }
+
+    void OnMouseDrag()
+    {
+         // Suivre le déplacement de la souris/du doigt en tenant compte de l'offset
+        Vector3 currentScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, mainCamera.WorldToScreenPoint(transform.position).z);
+        Vector3 currentPosition = mainCamera.ScreenToWorldPoint(currentScreenPoint) + offset;
+
+        // Limiter les valeurs de X et Y aux limites définies
+        currentPosition.x = Mathf.Clamp(currentPosition.x, -1.21f, 1.21f);
+        currentPosition.y = Mathf.Clamp(currentPosition.y, -2.11f, 2.75f);
+
+        // Mettre à jour la position de l'objet
+        transform.position = currentPosition;
     }
 }
