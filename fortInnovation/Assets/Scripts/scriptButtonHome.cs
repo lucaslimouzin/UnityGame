5fbd471761c698d4877fc405caee865973dd2177
Givros <69457849+lucaslimouzin@users.noreply.github.com>
2024-02-05 16:33:07 +0100

    ajout panel settings

---
+++ fortInnovation/Assets/Scripts/scriptButtonHome.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scriptButtonHome : MonoBehaviour
+{
+    public GameObject panelUi_Move;
+    public GameObject panelUi_Jump;
+
+
+    public void activeUi(){
+        MainGameManager.Instance.panelUiMobile = !MainGameManager.Instance.panelUiMobile;
+        if (MainGameManager.Instance.panelUiMobile){
+            Cursor.visible = true;
+            panelUi_Jump.SetActive(true);
+            panelUi_Move.SetActive(true);
+        } else {
+            panelUi_Jump.SetActive(false);
+            panelUi_Move.SetActive(false);
+        }
+    }
+    
+}
