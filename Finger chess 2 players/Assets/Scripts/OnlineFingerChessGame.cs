c98ca19e3dde1508069d1569ec148f6c64c6dd6d
Givros <limouzin.lucas@gmail.com>
2024-02-17 20:03:24 +0100

    ajout du text de début

--- Finger chess 2 players/Assets/Scripts/OnlineFingerChessGame.cs
+++ Finger chess 2 players/Assets/Scripts/OnlineFingerChessGame.cs
@@ -11,14 +11,21 @@ public class OnlineFingerChessGame : MonoBehaviour
     public Button buttonRightWhite;
     public Button buttonLeftBlack;
     public Button buttonRightBlack;
+    private Button buttonLeftPlayerLocal;
+    private Button buttonRightPlayerLocal;
+    private Button buttonLeftPlayerAdverse;
+    private Button buttonRightPlayerAdverse;
 
     public RectTransform backgroundWhite;
     public RectTransform backgroundBlack;
+
+    public TextMeshProUGUI textInfo; // Référence à l'objet TextMeshProUGUI
 
     void Start()
     {
         string playerColor = (string)PhotonNetwork.LocalPlayer.CustomProperties["playerColor"];
         InitializeBackgrounds(playerColor);
+        
     }
 
     void InitializeBackgrounds(string playerColor)
@@ -29,12 +36,25 @@ public class OnlineFingerChessGame : MonoBehaviour
             // Configure les backgrounds pour le joueur blanc
             SetRectTransform(backgroundWhite, 500, 0, 0);
             SetRectTransform(backgroundBlack, 0, 500, 0);
+            //faire en sorte que les boutons blancs soient aux joueur local et noirs à l'adversaire
+            buttonLeftPlayerLocal = buttonLeftWhite;
+            buttonRightPlayerLocal = buttonRightWhite;
+            buttonLeftPlayerAdverse = buttonLeftBlack;
+            buttonLeftPlayerAdverse = buttonLeftBlack;
+
+            UpdateTextInfo("À vous de commencer !");
         }
         else if (playerColor == "noir")
         {
             // Configure les backgrounds pour le joueur noir
             SetRectTransform(backgroundWhite, 0, 500, 180);
             SetRectTransform(backgroundBlack, 500, 0, 180);
+            //faire en sorte que les boutons noirs soient aux joueur local et blancs à l'adversaire
+            buttonLeftPlayerLocal = buttonLeftBlack;
+            buttonRightPlayerLocal = buttonRightBlack;
+            buttonLeftPlayerAdverse = buttonLeftWhite;
+            buttonLeftPlayerAdverse = buttonLeftWhite;
+            UpdateTextInfo("À votre adversaire de commencer ! ");
         }
     }
 
@@ -48,7 +68,13 @@ public class OnlineFingerChessGame : MonoBehaviour
         rectTransform.localEulerAngles = new Vector3(rectTransform.localEulerAngles.x, rectTransform.localEulerAngles.y, zRotation);
     }
 
-
+    void UpdateTextInfo(string message)
+    {
+        if (textInfo != null)
+        {
+            textInfo.text = message; // Met à jour le texte de l'objet TextMeshProUGUI
+        }
+    }
 
 
 
