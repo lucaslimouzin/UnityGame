9fd364d44f7993a3a131a57a4b7771f456058799
Givros <69457849+lucaslimouzin@users.noreply.github.com>
2024-03-19 11:40:10 +0100

    ajout des gameObject UI questions/instructions

--- fortInnovation/Assets/Scripts/GestionCinematiques.cs
+++ fortInnovation/Assets/Scripts/GestionCinematiques.cs
@@ -14,7 +14,7 @@ public class GestionCinematiques : MonoBehaviour
         GameObject buttonSuivant = GameObject.Find("ButtonSuivant");
         buttonSuivant.SetActive(false);
         // Commencez une coroutine pour attendre 3 secondes
-        StartCoroutine(MakeButtonVisibleAfterDelay(buttonSuivant, 3.0f));
+        StartCoroutine(MakeButtonVisibleAfterDelay(buttonSuivant, 6.0f));
         panelCinematiqueIntro.SetActive(false);
         switch (MainGameManager.Instance.cinematiqueEnCours){
             case "Introduction":
--- fortInnovation/Assets/Scripts/ScriptPanelRoom.cs
+++ fortInnovation/Assets/Scripts/ScriptPanelRoom.cs
@@ -26,6 +26,7 @@ public class ScriptPanelRoom : MonoBehaviour
         if (thirdPersonController != null)
         {
             thirdPersonController.enabled = true;
+            Debug.Log("oui");
         }
     }
 }
