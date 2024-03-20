02ee447b5823341c2813fa653e060d40c5cfd2c8
Givros <69457849+lucaslimouzin@users.noreply.github.com>
2024-03-20 16:00:07 +0100

    ajout fullscreen

--- fortInnovation/Assets/Scripts/scriptButtonHome.cs
+++ fortInnovation/Assets/Scripts/scriptButtonHome.cs
@@ -5,25 +5,18 @@ using UnityEngine;
 public class scriptButtonHome : MonoBehaviour
 {
     public GameObject panelUi_Move;
-    public GameObject panelUi_Jump;
+
 
 
     public void activeUi(){
         MainGameManager.Instance.panelUiMobile = !MainGameManager.Instance.panelUiMobile;
         if (MainGameManager.Instance.panelUiMobile){
-            
-            panelUi_Jump.SetActive(true);
             panelUi_Move.SetActive(true);
         } else {
-            panelUi_Jump.SetActive(false);
             panelUi_Move.SetActive(false);
         }
     }
 
-    //pour mettre en plein écran
-    public void ToggleFullScreen()
-    {
-        Screen.fullScreen = !Screen.fullScreen;
-    }
+    
     
 }
