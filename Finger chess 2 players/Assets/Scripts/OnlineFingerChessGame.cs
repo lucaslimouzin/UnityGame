f1d8762a68e83c965b639be79c7d0707b3977feb
Givros <69457849+lucaslimouzin@users.noreply.github.com>
2024-08-13 15:42:33 +0200

    modif version online

--- Finger chess 2 players/Assets/Scripts/OnlineFingerChessGame.cs
+++ Finger chess 2 players/Assets/Scripts/OnlineFingerChessGame.cs
@@ -15,20 +15,33 @@ public class OnlineFingerChessGame : MonoBehaviour
     private Button buttonRightPlayerLocal;
     private Button buttonLeftPlayerAdverse;
     private Button buttonRightPlayerAdverse;
+    public Image mainDroiteWhiteImage;
+    public Image mainGaucheWhiteImage;
+    public Image mainDroiteBlackImage;
+    public Image mainGaucheBlackImage;
+    private Image mainDroitePlayerLocalImage;
+    private Image mainGauchePlayerLocalImage;
+    private Image mainDroitePlayerAdverseImage;
+    private Image mainGauchePlayerAdverseImage;
 
     public RectTransform backgroundWhite;
     public RectTransform backgroundBlack;
 
     public TextMeshProUGUI textInfo; // Référence à l'objet TextMeshProUGUI
 
+    private bool isPlayerTurn;
+    private bool isTransferDone = false;
+    public Sprite[] spritesDoigts;
+
     void Start()
     {
         string playerColor = (string)PhotonNetwork.LocalPlayer.CustomProperties["playerColor"];
-        InitializeBackgrounds(playerColor);
+        InitializeBackgroundsAndImages(playerColor);
         
     }
 
-    void InitializeBackgrounds(string playerColor)
+    // ------ Partie Initialisation du jeu -----------
+    void InitializeBackgroundsAndImages(string playerColor)
     {
   
         if (playerColor == "blanc")
@@ -40,9 +53,15 @@ public class OnlineFingerChessGame : MonoBehaviour
             buttonLeftPlayerLocal = buttonLeftWhite;
             buttonRightPlayerLocal = buttonRightWhite;
             buttonLeftPlayerAdverse = buttonLeftBlack;
-            buttonLeftPlayerAdverse = buttonLeftBlack;
+            buttonRightPlayerAdverse = buttonRightBlack;
+            //de même pour les images des boutons
+            mainDroitePlayerLocalImage = mainDroiteWhiteImage;
+            mainGauchePlayerLocalImage = mainGaucheWhiteImage;
+            mainDroitePlayerAdverseImage = mainDroiteBlackImage;
+            mainGauchePlayerAdverseImage = mainGaucheBlackImage;
 
             UpdateTextInfo("À vous de commencer !");
+            isPlayerTurn = playerColor == "blanc";
         }
         else if (playerColor == "noir")
         {
@@ -53,9 +72,72 @@ public class OnlineFingerChessGame : MonoBehaviour
             buttonLeftPlayerLocal = buttonLeftBlack;
             buttonRightPlayerLocal = buttonRightBlack;
             buttonLeftPlayerAdverse = buttonLeftWhite;
-            buttonLeftPlayerAdverse = buttonLeftWhite;
+            buttonRightPlayerAdverse = buttonRightWhite;
+            //de même pour les images des boutons
+            mainDroitePlayerLocalImage = mainDroiteBlackImage;
+            mainGauchePlayerLocalImage = mainGaucheBlackImage;
+            mainDroitePlayerAdverseImage = mainDroiteWhiteImage;
+            mainGauchePlayerAdverseImage = mainGaucheWhiteImage;
+
             UpdateTextInfo("À votre adversaire de commencer ! ");
+            isPlayerTurn = playerColor == "noir";
         }
+
+        InitButtonAndImage(buttonRightPlayerLocal, mainDroitePlayerLocalImage, 1);
+        InitButtonAndImage(buttonLeftPlayerLocal, mainGauchePlayerLocalImage, 1);
+        InitButtonAndImage(buttonRightPlayerAdverse, mainDroitePlayerAdverseImage, 1);
+        InitButtonAndImage(buttonLeftPlayerAdverse, mainGauchePlayerAdverseImage, 1);
+
+    }
+
+    void InitButtonAndImage(Button button, Image image, int nombreDoigts)
+    {
+        SetFingerCount(button, nombreDoigts);
+        //on appelle la méthode qui gère l'opacité
+        SetButtonOpacity(button, 0f);
+    }
+
+    void SetFingerCount(Button button, int nombreDoigts)
+    {
+        Image buttonImage = null;
+
+        // Trouver l'image associ�e au bouton
+        if (button == buttonRightPlayerLocal)
+        {
+            buttonImage = mainDroitePlayerLocalImage;
+        }
+        else if (button == buttonLeftPlayerLocal)
+        {
+            buttonImage = mainGauchePlayerLocalImage;
+        }
+        else if (button == buttonRightPlayerAdverse)
+        {
+            buttonImage = mainDroitePlayerAdverseImage;
+        }
+        else if (button == buttonLeftPlayerAdverse)
+        {
+            buttonImage = mainGauchePlayerAdverseImage;
+        }
+
+        // V�rifier si l'image est valide et le nouveau nombre de doigts est dans la plage des sprites
+        if (buttonImage != null && nombreDoigts >= 0 && nombreDoigts <= 5)
+        {
+            // Mettre � jour le sprite de l'image associ�e au bouton
+            buttonImage.sprite = spritesDoigts[nombreDoigts - 1];
+        }
+        else
+        {
+            // Mettre � jour le sprite de l'image associ�e au bouton
+            buttonImage.sprite = spritesDoigts[5];
+        }
+    }
+
+
+    void SetButtonOpacity(Button button, float opacity)
+    {
+        Color color = button.image.color;
+        color.a = opacity;
+        button.image.color = color;
     }
 
     void SetRectTransform(RectTransform rectTransform, float top, float bottom, float zRotation)
@@ -68,6 +150,7 @@ public class OnlineFingerChessGame : MonoBehaviour
         rectTransform.localEulerAngles = new Vector3(rectTransform.localEulerAngles.x, rectTransform.localEulerAngles.y, zRotation);
     }
 
+
     void UpdateTextInfo(string message)
     {
         if (textInfo != null)
@@ -76,6 +159,7 @@ public class OnlineFingerChessGame : MonoBehaviour
         }
     }
 
+    // ---------------- Partie Gameplay --------------
 
 
     //fin app
